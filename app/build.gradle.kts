2e1d100f8ef47f92589bff570038915880d535ef
Alex Osinniy <appsosinniy@gmail.com>
2020-12-29 22:17:28 +0200

    Fix ktlintKotlinScriptCheck #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,6 @@ android {
             val keystorePropertiesFile = rootProject.file("keystore.properties")
             val keystoreProperties = Properties()
             keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
             storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
             keyAlias = keystoreProperties["keyAlias"] as String
