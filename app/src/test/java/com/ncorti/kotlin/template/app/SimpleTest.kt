cdbf0d8eeee8932f42768c99597f1b0bea2884d9
Alex Osinniy <appsosinniy@gmail.com>
2020-12-31 00:07:42 +0200

    Create SimpleTest.kt

---
+++ app/src/test/java/com/ncorti/kotlin/template/app/SimpleTest.kt
@@ -0,0 +1,13 @@
+package com.ncorti.kotlin.template.app
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class SimpleTest {
+
+    @Test
+    fun test() {
+        assetEquals(4, 2 + 2)
+    }
+
+}
